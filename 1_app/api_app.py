02c1f32e9e7418f23e82913444e95e97304a9ce8
Kevin Talbert <ktalbert@cloudera.com>
2023-11-03 15:57:57 -0400

    Updated AMP

--- 1_app/api_app.py
+++ 1_app/api_app.py
@@ -1,12 +1,13 @@
 from typing import Any, Union, Optional
 from fastapi import FastAPI
 from fastapi import HTTPException
+from pydantic import BaseModel
 import os
 import threading
 import asyncio
 import subprocess
+import uvicorn
 from typing import Any, Union, Optional
-from sentence_transformers import SentenceTransformer ## Needed??
 import chromadb
 from chromadb.utils import embedding_functions
 
@@ -17,7 +18,7 @@ EMBEDDING_FUNCTION = embedding_functions.SentenceTransformerEmbeddingFunction(mo
 COLLECTION_NAME = os.getenv('COLLECTION_NAME')
 
 print("initialising Chroma DB connection...")
-chroma_client = chromadb.Client()
+chroma_client = chromadb.PersistentClient()
 print("Chroma DB initialised")
 
 print(f"Getting '{COLLECTION_NAME}' as object...")
@@ -36,89 +37,98 @@ print('Total number of embeddings in Chroma DB index is {}.'.format(current_coll
 app = FastAPI()
 
 # Helper function for generating responses for the Chroma DB
-def get_responses(question):
+def get_responses(question, max_results):
+    results = collection.query(query_texts=question, n_results=max_results) 
+    print(results)
+    # Return text of the nearest knowledge base chunk(s)
+    #response = load_context_chunk_from_data(matching_files[0])
     
-    # Get nearest knowledge base chunk for a user question from a vector db
-    response = get_nearest_chunk_from_chroma_vectordb(index, question)
-    
-    return response
+    return results
+
 
 # Helper function for adding documents to the Chroma DB
-def upsert_document(collection, document, metadata, classification):
+def upsert_document(document, metadata=None, classification="public", file_path=None):
     
-    # Get nearest knowledge base chunk for a user question from a vector db
-    response = upsert_to_chroma_vectordb(index, question)
-    
+    # Push document to Chroma vector db
+    if file_path is not None:
+        response = collection.add(
+            documents=[document],
+            metadatas=[{"classification": classification}],
+            ids=[file_path]
+        )
+    else:
+        response = collection.add(
+            documents=[document],
+            metadatas=[{"classification": classification}],
+            ids=mystring[:50]
+        )
     return response
 
-# Get embeddings for a user question and query Pinecone vector DB for nearest knowledge base chunk
-def get_nearest_chunk_from_chroma_vectordb(index, question):
-    # Generate embedding for user question with embedding model
-    retriever = SentenceTransformer(EMBEDDING_MODEL_REPO)
-    xq = retriever.encode([question]).tolist()
-    xc = index.query(xq, top_k=5,
-                 include_metadata=True)
-    
-    matching_files = []
-    for match in xc['matches']:
-        # extract the 'file_path' within 'metadata'
-        file_path = match['metadata']['file_path']
-        matching_files.append(file_path)
-
-    # Return text of the nearest knowledge base chunk 
-    # Note that this ONLY uses the first matching document for semantic search. matching_files holds the top results so you can increase this if desired.
-    response = load_context_chunk_from_data(matching_files[0])
-    return response
-  
 # Return the Knowledge Base doc based on Knowledge Base ID (relative file path)
 def load_context_chunk_from_data(id_path):
     with open(id_path, "r") as f: # Open file in read mode
         return f.read()
 
-# Get embeddings for a user question and query Chroma vector DB for nearest knowledge base chunk
-def get_nearest_chunk_from_chroma_vectordb(collection, document, metadata, classification):
-    
-    
-    return "Successful upload"
-    
-    
-    
 @app.get("/")
 def status_gpu_check() -> dict[str, str]:
-    gpu_msg = "Available" if tf.test.is_gpu_available() else "Unavailable"
+    chroma_status = "Chroma Vector DB READY" if collection else "Unavailable"
     return {
-        "status": "I am ALIVE!",
-        "gpu": gpu_msg
+        "server_status": "Web server is ALIVE",
+        "chroma_status": chroma_status
     }
+
+# This defines the data json format expected for the endpoint, change as needed
+class TextInput(BaseModel):
+    inputs: str
+    parameters: Union[dict[str, Any], None]
 
 @app.post("/upsert")
 def upsert_endpoint(data: TextInput) -> dict[str, str]:
     try:
-        question = data.inputs
+        document = data.inputs
         params = data.parameters or {}
+        print(str(document))
         
         if 'metadata' in params:
             metadata = int(params['metadata'])
+            print("Using: "+ str(metadata))
+        else:
+            metadata = None
             
         if 'classification' in params:
             classification = int(params['classification'])
-            
-        print(str(question))
-        print("Using: "+ str(metadata))
+        else:
+            classification = "public"
+        
+        if 'file_path' in params:
+            file_path = int(params['file_path'])
+            print("Using: "+ str(file_path))
+        else:
+            file_path = None
+
         print("Using: "+ str(classification))
             
-        res = upsert_document(collection, document, metadata, classification)
+        res = upsert_document(document, metadata, classification, file_path)
 
         return {"response": res}
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@app.post("/generate")
-def generate_endpoint(data: TextInput) -> dict[str, str]:
+@app.post("/query")
+def query_endpoint(data: TextInput) -> dict[str, str]:
     try:
         question = data.inputs
-        res = get_responses(question)
+        params = data.parameters or {}
+        
+        if 'max_results' in params:
+            max_results = int(params['max_results'])
+        else:
+            max_results = 1
+            
+        res = get_responses(question, max_results)
         return {"response": res}
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
         
