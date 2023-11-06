f83cb69ed2e7d6b92e949e86558db26cc2438187
Kevin Talbert <ktalbert@cloudera.com>
2023-11-05 23:18:24 -0500

    Added debug log suppression

--- 1_session-setup-chroma-db/setup-chroma.sh
+++ 1_session-setup-chroma-db/setup-chroma.sh
@@ -20,17 +20,11 @@ sed -i '/logger = logging\.getLogger(__name__)/d' "$FILE"
 LINE1="__import__('pysqlite3')"
 LINE2="import sys"
 LINE3="sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')"
-LINE4="import logging"
-LINE5="logger = logging.getLogger(__name__)"
-LINE6="logger.setLevel(logging.CRITICAL)"
 
 # Write the new lines to the temp file
 echo "$LINE1" > "$TEMP_FILE"
 echo "$LINE2" >> "$TEMP_FILE"
 echo "$LINE3" >> "$TEMP_FILE"
-echo "$LINE4" >> "$TEMP_FILE"
-echo "$LINE5" >> "$TEMP_FILE"
-echo "$LINE6" >> "$TEMP_FILE"
 
 # Append the original file content to the temp file
 cat "$FILE" >> "$TEMP_FILE"
@@ -39,3 +33,11 @@ cat "$FILE" >> "$TEMP_FILE"
 mv "$TEMP_FILE" "$FILE"
 
 echo "Lines added successfully."
+
+# Define the path to the YAML file
+yaml_file="/home/cdsw/.local/lib/python3.10/site-packages/chromadb/log_config.yml"
+
+# Use sed to find the pattern and replace the line
+# This command looks for a line containing 'uvicorn:' followed by any number of spaces and 'level: INFO'
+# and replaces it with 'level: DEBUG'
+sed -i '/uvicorn:/{n;s/level: INFO/level: ERROR/;}' "$yaml_file"
